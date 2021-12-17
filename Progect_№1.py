ea965b9be74244d946edc363faa7db7a85e8f6d4
Matsu <76900768+IIMatsuII@users.noreply.github.com>
2021-12-17 17:20:55 +0500

    Add files via upload

--- Progect_№1.py
+++ Progect_№1.py
@@ -1,44 +1,45 @@
 from random import *
-condition = True
-while condition:
-    conter = 0
-    n = int(input("Введите границу: "))
-    num = randint(1, n)
-    print("Добро пожаловать в числовую угадайку: ")
+def guess_game():
+    condition = True
+    while condition:
+        conter = 0
+        n = int(input("Введите границу: "))
+        num = randint(1, n)
+        print("Добро пожаловать в числовую угадайку: ")
 
-    def is_valid(num):
-        if num.isdigit():
-            num = int(num)
-            if 1 <= num <= 100:
-                return True
-            else:
-                return False
+        def is_valid(num):
+            if num.isdigit():
+                num = int(num)
+                if 1 <= num <= 100:
+                    return True
+                else:
+                    return False
 
-    while True:
-        numbers = input(f"Введите число от 1 до {n}: ")
-        if is_valid(numbers):
-            numbers = int(numbers)
-            if numbers < num:
-                print("Ваше число меньше заданного, попробуйте еще разок: ")
-                conter += 1
-            elif numbers > num:
-                print("Ваше число больше заданного, попробуйте еще разок")
-                conter += 1
-            elif numbers == num:
-                print("Вы угадали, поздравляем:")
-                break
-            else:
-                print("Может быть введем число от 1 до 100 :")
-    print(f"Вы пытались угадать: {conter} раз")
-    print("Хотетие еще раз сыграть: ")
-    key = input("Введите ваш ответ: ")
-    if key == "yes":
-        continue
-    elif key == "no":
-        print(f"Спасибо, что играли в числовую угадайку. Еще увидимся...")
-        break
+        while True:
+            numbers = input(f"Введите число от 1 до {n}: ")
+            if is_valid(numbers):
+                numbers = int(numbers)
+                if numbers < num:
+                    print("Ваше число меньше заданного, попробуйте еще разок: ")
+                    conter += 1
+                elif numbers > num:
+                    print("Ваше число больше заданного, попробуйте еще разок")
+                    conter += 1
+                elif numbers == num:
+                    print("Вы угадали, поздравляем:")
+                    break
+                else:
+                    print("Может быть введем число от 1 до 100 :")
+        print(f"Вы пытались угадать: {conter} раз")
+        print("Хотетие еще раз сыграть: ")
+        key = input("Введите ваш ответ: ")
+        if key == "yes":
+            continue
+        elif key == "no":
+            print(f"Спасибо, что играли в числовую угадайку. Еще увидимся...")
+            break
 
-
+guess_game()
 
 
 
